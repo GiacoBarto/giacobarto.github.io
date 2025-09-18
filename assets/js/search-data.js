582caca97cb7f49f943c166dcbcc4434a625666f
GiacoBarto <141183186+GiacoBarto@users.noreply.github.com>
2025-09-18 16:58:15 +0000

    Deploying to gh-pages from @ GiacoBarto/giacobarto.github.io@a33c5fa696a80f718f93d43cda09c62367b0d111 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -422,9 +422,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-active-nematics",
+          title: 'Active nematics',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
